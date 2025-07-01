7436242fcb609810226c314cd6e2bdd67354056b
sthyt99 <170448040@ccalumni.meijo-u.ac.jp>
2025-07-01 20:27:02 +0900

    初回コミット

---
+++ src/main/java/com/example/health_app/HealthAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.health_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/health_app/HealthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.health_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
